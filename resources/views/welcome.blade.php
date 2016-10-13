595bdc4401236ff1176c0d8958d7514b8443665c
jmunizaga@github <jaime.munizaga.l@gmail.com>
2016-10-13 20:38:42 -0300

    push test a master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Bienvenido a www.Verdor.cl!
+                    == Verdor ==
                 </div>
             </div>
         </div>
