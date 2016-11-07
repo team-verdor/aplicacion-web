97029d2a68dc47a5ea46429b69fc5621460a5765
Arie CwHat <arie.cwhat@gmail.com>
2016-11-07 08:28:57 -0300

    Anade la plantilla frontend.

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/autodeploy', 'GitAutoDeploy@doPull');
 Route::post('/autodeploy', 'GitAutoDeploy@doPull');
 
 Route::get('/test', 'RaspberryTesting@printHello');
+
+Route::any('/echo', function (Request $request) {
+    dd($request->all());
+});
